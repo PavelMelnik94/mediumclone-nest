30a0ebc63da49b8ef7f8c81173acf973b54188a1
pavelMelnik <p.melnik@intelico.su>
2023-02-05 23:40:34 +0300

    feature | add entity tag

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -7,6 +7,8 @@ const ormConfig: ConnectionOptions = {
 	username: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
+	synchronize: true,
+	entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 };
 
 export default ormConfig;

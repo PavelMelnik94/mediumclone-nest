2d70ffaddda98081b7f3c4f9e42e172b5cfd3e80
pavelMelnik <p.melnik@intelico.su>
2023-02-06 00:54:28 +0300

    feature | add repository

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,12 +1,17 @@
 import { ConnectionOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
 
+config();
+
+const configService = new ConfigService();
 const ormConfig: ConnectionOptions = {
 	type: 'postgres',
-	host: process.env.DB_HOST || 'localhost',
-	port: Number(process.env.DB_PORT) || 5432,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
+	host: configService.get('DB_HOST') || 'localhost',
+	port: configService.get('DB_PORT') || 5432,
+	username: configService.get('DB_USER'),
+	password: configService.get('DB_PASSWORD'),
+	database: configService.get('DB_NAME'),
 	synchronize: true,
 	entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-if (!process.env.IS_TS_NODE) {
+if (process.env.IS_TS_NODE) {
 	require('module-alias/register');
 }
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';

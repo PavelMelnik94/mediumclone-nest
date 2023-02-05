db143bdeb5bbef37d4140e481252369a83387371
pavelMelnik <p.melnik@intelico.su>
2023-02-05 23:06:21 +0300

    feature | rewtire paths

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TagService {
 	findAll(): string[] {
-		return ['tag1', 'tag2', 'tag3'];
+		return ['tag1', 'tag2', 'tag3', 'tag4'];
 	}
 }

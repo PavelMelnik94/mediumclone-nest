fd13a48194e32d9365237ac5385b06b2c41ac0ea
pavelMelnik <p.melnik@intelico.su>
2023-02-06 23:13:57 +0300

    feature | add throw error if username or email already exist

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from './user.entity';
 import { ConfigService } from '@nestjs/config';
@@ -10,6 +10,26 @@ const configService = new ConfigService();
 @Injectable()
 export class UserService {
 	async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+		const isEmailAlreadyExist = await PostgresDataSource.manager.findOne(
+			UserEntity,
+			{
+				where: { email: createUserDto.email },
+			},
+		);
+
+		const isUsernameAlreadyExist = await PostgresDataSource.manager.findOne(
+			UserEntity,
+			{
+				where: { username: createUserDto.username },
+			},
+		);
+
+		if (isEmailAlreadyExist || isUsernameAlreadyExist) {
+			throw new HttpException(
+				'Email or username already exist',
+				HttpStatus.UNPROCESSABLE_ENTITY,
+			);
+		}
 		const newUser = new UserEntity();
 		Object.assign(newUser, createUserDto);
 		return PostgresDataSource.manager.save(newUser);

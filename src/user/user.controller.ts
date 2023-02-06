a6685e770bbb94c856dc77c177f32d3fb610ac78
pavelMelnik <p.melnik@intelico.su>
2023-02-06 23:06:01 +0300

    feature | add validation pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,12 +3,15 @@ import { Body, Post } from '@nestjs/common/decorators';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { UsePipes } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 @Controller()
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Post('users')
+	@UsePipes(new ValidationPipe())
 	async createUser(
 		@Body('user') createUserDto: CreateUserDto,
 	): Promise<UserResponseInterface> {

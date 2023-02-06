290884cee48d1200339788e0c0dd3d295328ba8b
pavelMelnik <p.melnik@intelico.su>
2023-02-06 22:57:10 +0300

    feature | add user interface and user type. replace return type to correct type

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,13 +2,17 @@ import { Controller } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Controller()
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Post('users')
-	async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
-		return this.userService.createUser(createUserDto);
+	async createUser(
+		@Body('user') createUserDto: CreateUserDto,
+	): Promise<UserResponseInterface> {
+		const user = await this.userService.createUser(createUserDto);
+		return this.userService.buildUserResponse(user);
 	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from './user.entity';
+import { ConfigService } from '@nestjs/config';
+import { sign } from 'jsonwebtoken';
 import PostgresDataSource from '@app/config/orm.config';
+import { UserResponseInterface } from './types/userResponse.interface';
 
+const configService = new ConfigService();
 @Injectable()
 export class UserService {
 	async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
 		const newUser = new UserEntity();
 		Object.assign(newUser, createUserDto);
 		return PostgresDataSource.manager.save(newUser);
+	}
+
+	generateJWT(user: UserEntity): string {
+		console.log(user);
+		return sign(
+			{
+				id: user.id,
+				username: user.username,
+				email: user.email,
+			},
+			configService.get('JWT_SECRET'),
+		);
+	}
+
+	buildUserResponse(user: UserEntity): UserResponseInterface {
+		return {
+			user: {
+				...user,
+				token: this.generateJWT(user),
+			},
+		};
 	}
 }

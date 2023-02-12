b580002700a309aa878c5d5ba4d3a96035f4ccf8
pavelMelnik <p.melnik@intelico.su>
2023-02-12 16:14:52 +0300

    feature | add article dto

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,12 +1,21 @@
 import { Controller } from '@nestjs/common';
-import { Post } from '@nestjs/common/decorators';
+import { Body, Post, UseGuards } from '@nestjs/common/decorators';
 import { ArticleService } from './article.service';
+import { AuthGuard } from '../user/guards/auth.guard';
+import { UserEntity } from '../user/user.entity';
+import { User } from '../user/decorators/user.decorator';
+import { CreateArticleDto } from './dto/create-article.dto';
 
 @Controller('article')
 export class ArticleController {
 	constructor(private readonly articleService: ArticleService) {}
+
 	@Post()
-	async createArticle() {
-		return this.articleService.createArticle();
+	@UseGuards(AuthGuard)
+	async create(
+		@User() currentUser: UserEntity,
+		@Body('article') createArticleDto: CreateArticleDto,
+	): Promise<any> {
+		return this.articleService.createArticle(currentUser, createArticleDto);
 	}
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from '../user/user.entity';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { ArticleEntity } from './article.entity';
+import PostgresDataSource from '@app/config/orm.config';
 
 @Injectable()
 export class ArticleService {
-	async createArticle() {
-		return 'createArticle';
+	async createArticle(
+		currentUser: UserEntity,
+		createArticleDto: CreateArticleDto,
+	): Promise<ArticleEntity> {
+		const article = new ArticleEntity();
+		Object.assign(article, createArticleDto);
+
+		if (article.tagList) {
+			article.tagList = [];
+		}
+
+		article.author = currentUser;
+
+		return PostgresDataSource.manager.save(article);
 	}
 }

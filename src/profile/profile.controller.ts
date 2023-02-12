d3bdaa7c242d86c5fb4bea2054a5cc394bb06a93
pavelMelnik <p.melnik@intelico.su>
2023-02-12 20:56:54 +0300

    feature | add follow action

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { User } from '../user/decorators/user.decorator';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
 import { ProfileService } from './profile.service';
+import { AuthGuard } from '../user/guards/auth.guard';
 
 @Controller('profiles')
 export class ProfileController {
@@ -12,6 +13,20 @@ export class ProfileController {
 		@Param('username') profileUserName: string,
 	): Promise<ProfileResponseInterface> {
 		const profile = await this.profileService.getProfile(
+			currentUserId,
+			profileUserName,
+		);
+
+		return this.profileService.buildProfileResponse(profile);
+	}
+
+	@Post(':username/follow')
+	@UseGuards(AuthGuard)
+	async followProfile(
+		@User('id') currentUserId: number,
+		@Param('username') profileUserName: string,
+	): Promise<ProfileResponseInterface> {
+		const profile = await this.profileService.followProfile(
 			currentUserId,
 			profileUserName,
 		);
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -3,6 +3,7 @@ import { ProfileResponseInterface } from './types/profileResponse.interface';
 import { ProfileType } from './types/profile.type';
 import PostgresDataSource from '../config/orm.config';
 import { UserEntity } from '../user/user.entity';
+import { FollowEntity } from './follow.entity';
 
 @Injectable()
 export class ProfileService {
@@ -22,6 +23,41 @@ export class ProfileService {
 			...user,
 			following: false,
 		};
+	}
+
+	async followProfile(
+		currentUserId: number,
+		profileUserName: string,
+	): Promise<ProfileType> {
+		const user = await PostgresDataSource.manager.findOne(UserEntity, {
+			where: { username: profileUserName },
+		});
+
+		if (!user) {
+			throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+		}
+
+		if (currentUserId === user.id) {
+			throw new HttpException(
+				'You cannot follow yourself',
+				HttpStatus.BAD_REQUEST,
+			);
+		}
+
+		const follow = await PostgresDataSource.manager.findOne(FollowEntity, {
+			where: {
+				followerId: currentUserId,
+				followingId: user.id,
+			},
+		});
+
+		if (!follow) {
+			const followToCreate = new FollowEntity();
+			followToCreate.followerId = currentUserId;
+			followToCreate.followingId = user.id;
+			await PostgresDataSource.manager.save(followToCreate);
+		}
+		return { ...user, following: true };
 	}
 
 	buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
